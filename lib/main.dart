461c8af5235de2d72ac321955ad0520e90a9fe35
Mayank Batra <mayankbatra51@gmail.com>
2023-01-11 14:52:34 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:fashion_store/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/nav_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primaryColor,
+        buttonTheme: const ButtonThemeData(
+          buttonColor: AppColors.buttonColor,
+          textTheme: ButtonTextTheme.primary,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: AppColors.secondaryColor,
+        ),
+      ),
+      home: const NavBar(),
+    );
+  }
+}
